866467b5b49bf6fb3387a23eb94075ff3ffa4d86
ssergioogj <100522264@alumnos.uc3m.es>
2025-10-08 19:09:16 +0200

    implementacion de la blockchain
    Falta que garantice que el bloque no puede ser modificado y aplicar una lista simplemente enlazada

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,10 +1,15 @@
+import datetime
+import hashlib
+from time import timezone
+import json
+
 class Block:
-    def __init__(self, index, previous_hash, timestamp, vote, hash):
+    def __init__(self, index, previous_hash, vote):
         self.__index = index
         self.__previous_hash = previous_hash
-        self.__timestamp = timestamp
+        self.__timestamp = datetime.timestamp(datetime.now(datetime.timezone.utc))
         self.__vote = vote
-        self.__hash = hash
+        self.__hash = self.__calculate_hash()
 
     
     @property
@@ -22,6 +27,22 @@ class Block:
     @property
     def hash(self):
         return self.__hash
+    
+
+    def __calculate_hash(self):
+        """
+        Calcula el hash SHA-256 del contenido del bloque
+        (excepto el propio hash, para evitar bucle).
+        """
+        block_data = {
+            "index": self.__index,
+            "previous_hash": self.__previous_hash,
+            "timestamp": self.__timestamp,
+            "vote": self.__votes
+        }
+        # Convertimos el bloque a JSON ordenado y calculamos su hash
+        encoded = json.dumps(block_data, sort_keys=True).encode()
+        return hashlib.sha256(encoded).hexdigest()
     
     def __repr__(self):
         return f"Block(index={self.index}, previous_hash='{self.previous_hash}', timestamp={self.timestamp}, data='{self.data}', hash='{self.hash}')"
